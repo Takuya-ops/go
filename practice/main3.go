19fe9ffb6ac72ecec1d86eaa388281e073a71ad2
Takuya-ops <hora2112.ipu@gmail.com>
2023-10-26 23:36:39 +0900

    evenodd

---
+++ practice/main3.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// スライスを偶数と奇数に分ける
+func getEvenOddNumbers(numbers []int) ([]int, []int) {
+	var evenNumbers []int
+	var oddNumbers []int
+	for _, number := range numbers {
+		if number%2 == 0 {
+			evenNumbers = append(evenNumbers, number)
+		} else {
+			oddNumbers = append(oddNumbers, number)
+		}
+	}
+	return evenNumbers, oddNumbers
+}
+
+func main() {
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	evenNumbers, oddNumbers := getEvenOddNumbers(numbers)
+	fmt.Println("元のスライス：", numbers)
+	fmt.Println("偶数のスライス：", evenNumbers)
+	fmt.Println("奇数のスライス：", oddNumbers)
+}

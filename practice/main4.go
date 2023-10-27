ea0f1e8dc3449322f357a16756a8ce61e5550a52
Takuya-ops <hora2112.ipu@gmail.com>
2023-10-27 10:21:32 +0900

    偶数のみのスライス

---
+++ practice/main4.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// 偶数のみのスライスを作成
+func getEvenNumbers(numbers []int) []int {
+	var evenNumbers []int
+	for _, num := range numbers {
+		if num%2 == 0 {
+			evenNumbers = append(evenNumbers, num)
+		}
+	}
+	return evenNumbers
+}
+
+func main() {
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	evennumbers := getEvenNumbers(numbers)
+	fmt.Println("偶数のみのスライス:", evennumbers)
+}
